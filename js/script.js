9508c3522179406f2b6f22bd5465e4faf6b83e84
LucaPierantoni <luca.pier99@gmail.com>
2024-05-21 16:02:28 +0200

    Bonus 1 e 2 completi + aggiunto v-cloak

--- js/script.js
+++ js/script.js
@@ -1,11 +1,9 @@
 const { createApp } = Vue;
-
 createApp({
     data() {
         return {
-
             currentSlideIndex: 0,
-
+            
             slides: [
                 {
                     image: 'img/01.webp',
@@ -33,31 +31,31 @@ createApp({
                     text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
                 }
             ],
-
         }
     },
 
     methods: {
-
         nextSlide() {
-
             this.currentSlideIndex++;
-
             if(this.currentSlideIndex >= this.slides.length) {
                 this.currentSlideIndex = 0;
             }
-
         },
-
         prevSlide() {
-
             this.currentSlideIndex--;
-
             if(this.currentSlideIndex < 0) {
                 this.currentSlideIndex = this.slides.length - 1;
             }
-
+        },
+        changeSlide(newIndex) {
+            this.currentSlideIndex = newIndex;
         }
+    },
 
+    mounted() {
+
+        // avviare l'autoplay dello slideshow
+        const autoplayTimer = setInterval(this.nextSlide, 3000);
+    
     },
 }).mount("#app");
